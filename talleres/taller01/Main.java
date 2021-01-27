e32f902c4f7df6c735c966f800136cc6a5d5a754
Miguelco23 <60229768+Miguelco23@users.noreply.github.com>
2021-01-27 08:32:50 -0500

    Update Main.java

--- talleres/taller01/Main.java
+++ talleres/taller01/Main.java
@@ -3,14 +3,15 @@ public class Main
     public static void main(String[] args)
     {
         //Clase punto
-       /* Punto punto = new Punto(12,6);
+        Punto punto = new Punto(12,6);
         System.out.println("Clase punto: ");
-        System.out.println("cordenada X: "+punto.CordenadaX()+", Codenada Y: "+punto.CordenadaY());
-        System.out.println("Radio: "+ punto.Radio());
-        System.out.println("Angulo: "+ punto.Angulo());
-        System.out.println("Distancia: "+ punto.Distancia(6,3)+"\n");*/
+        System.out.println("cordenada X: "+punto.x()+", Codenada Y: "+punto.y());
+        System.out.println("Radio: "+ punto.radioPolar());
+        System.out.println("Angulo: "+ punto.anguloPolar());
+        System.out.println("Distancia: "+ punto.distanciaEuclidiana((6, 3))+"\n");
 
         //Clase Date
+        /*
         Fecha fecha = new Fecha(11,05,1999);
         System.out.println("Clase Date: ");
         System.out.println("Comparcion de fecha: "+ fecha.comparar(new Fecha(28,01,2021)));
@@ -19,12 +20,7 @@ public class Main
         System.out.println("Mes: "+ fecha.mes());
         System.out.println("Año: "+ fecha.anio()+"\n");
 
-        //clase contador
-        Contador contador = new Contador("Contador =");
-        System.out.println("Clase Contador: ");
-        System.out.println(contador.Incrementos(5,3));
-        System.out.println(contador.ValorActual());
-        System.out.println(contador.toString());
+        */
 
     }
 }
