9150b0befce36ecda0b1af94d4326c94bbe90cde
Miguelco23 <60229768+Miguelco23@users.noreply.github.com>
2021-02-10 08:31:00 -0500

    Update Taller3.java

--- talleres/taller03/Taller3.java
+++ talleres/taller03/Taller3.java
@@ -37,8 +37,15 @@ public class Taller3 {
 	*/	
 	
 	private static void torresDeHannoiAux(int n, int origen, int intermedio, int destino) {
-		//...
-	}
+		if(n == 1){   
+			System.out.println("Disk 1 from "+ origen + " to "+ destino);
+	    }
+		else {
+	    	torresDeHannoiAux(n-1, origen, destino, auxiliar);
+	    	System.out.println("Disk "+ n + " from "+ origen + " to "+ destino);
+	    	torresDeHannoiAux(n-1, intermedio, origen, destino);
+		}
+	}	
 		
 
 		
