ff1706e6e789fe607c83ec404df6ac996d2bcdac
Miguelco23 <60229768+Miguelco23@users.noreply.github.com>
2021-02-24 08:05:55 -0500

    Update Taller5.java

--- talleres/taller05/Taller5.java
+++ talleres/taller05/Taller5.java
@@ -34,7 +34,9 @@ public class Taller5 {
     * 
     */
     public static void mul (int num){
-      //...
+      for(int i=1;i<=10;i++){
+          System.out.println(num+" x "+i+" = "+(num*i));
+      }
     }
     
     
